af12bff256e405f198ea2decf08f89d876cc8dfa
Danijel Vlah <danijel.vlah@lama.hr>
2024-05-26 23:14:56 +0200

    admin can now bulk delete products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -7,6 +7,7 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { ToastrService } from 'ngx-toastr';
 import { AddProductComponent } from './add-product/add-product.component';
+import { AuthService } from '../../shared/services/authService/auth.service';
 
 @Component({
   selector: 'app-products',
@@ -16,15 +17,21 @@ import { AddProductComponent } from './add-product/add-product.component';
 export class ProductsComponent {
   constructor(
     private productService: ProductService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private authService: AuthService
   ) {}
   products!: Product[];
+  isAdmin!: boolean;
+  deleteMode: boolean = false;
+  selectedProductIds: string[] = [];
 
-  ngOnInit() {
+  async ngOnInit() {
     const $products = this.getProducts();
     $products.subscribe((products) => {
       this.products = [...products];
     });
+    const uid = await this.authService.getCurrentUserId();
+    this.isAdmin = await this.authService.isAdmin(uid);
   }
 
   addProduct() {
@@ -32,14 +39,47 @@ export class ProductsComponent {
   }
 
   getProductInfo(product: Product) {
-    const dialogRef = this.dialog.open(ProductDialogComponent, {
+    this.dialog.open(ProductDialogComponent, {
       data: product,
     });
+  }
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log('Dialog closed with result:', result);
+  toggleDeleteMode() {
+    this.deleteMode = !this.deleteMode;
+  }
+
+  toggleProductIdSelection(productId: string) {
+    const index = this.selectedProductIds.indexOf(productId);
+    if (index === -1) {
+      this.selectedProductIds.push(productId);
+    } else {
+      this.selectedProductIds.splice(index, 1);
+    }
+  }
+
+  selectAll() {
+    const productsLen = this.products.length;
+    if (productsLen === this.selectedProductIds.length) {
+      this.selectedProductIds = [];
+      return;
+    }
+
+    this.products.forEach((product) => {
+      const index = this.selectedProductIds.indexOf(product.id);
+      if (index === -1) {
+        this.selectedProductIds.push(product.id);
+      }
     });
-    //this.toastr.success('too', 'bravo');
+  }
+
+  deleteSelectedProducts() {
+    this.selectedProductIds.forEach((id) => {
+      this.productService.deleteProduct(id);
+    });
+  }
+
+  isProductSelected(productId: string): boolean {
+    return this.selectedProductIds.includes(productId);
   }
 
   getProducts(): Observable<Product[]> {
